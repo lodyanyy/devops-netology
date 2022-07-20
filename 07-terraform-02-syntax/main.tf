cf6ff6a2cc9ab6374a0c51df02562ff136baa261
lodyanyy <87534423+lodyanyy@users.noreply.github.com>
2022-07-20 15:45:15 +0400

    Update main.tf

--- 07-terraform-02-syntax/main.tf
+++ 07-terraform-02-syntax/main.tf
@@ -1,13 +1,40 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-    }
-  }
+provider "yandex" {
+  cloud_id  = "b1gt0lvva0m38u41d9e3"
+  folder_id = "b1gem8suj95s5qrc1va1"
+  zone      = "ru-central1-a"
 }
 
-provider "yandex" {
-  token     = "my_token_is_here"
-  cloud_id  = "my_cloud_id"
-  folder_id = "my_folder_id"
+resource "yandex_compute_image" "image" {
+  family = "centos-8"
+}
+
+resource "yandex_compute_instance" "vm1" {
+  name                      = "vm1"
+  zone                      = "ru-central1-a"
+  hostname                  = "vm1.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 1
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${yandex_compute_image.image.id}"
+      name        = "root-vm1"
+      type        = "network-nvme"
+      size        = "30"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+
 }
