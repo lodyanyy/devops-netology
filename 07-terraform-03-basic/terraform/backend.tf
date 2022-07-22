fef065db9de41127c7f4ab3c512ccfa5aef4982b
lodyanyy <87534423+lodyanyy@users.noreply.github.com>
2022-07-22 21:36:51 +0400

    Update backend.tf

--- 07-terraform-03-basic/terraform/backend.tf
+++ 07-terraform-03-basic/terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     region     = "ru-central1"
     key        = "path/terraform.tfstate"
     access_key = "YCAJEKjJBjqHd_5QuEuia0HQZ"
-    secret_key = "YCPEOk8D70lYdmt1jUX6gsjbrZ7yXRc2zoEB7HHX"
+    secret_key = "00000000000"
 
     skip_region_validation      = true
     skip_credentials_validation = true
